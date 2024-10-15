59b4f92277c6f124666ba9c35c9c44396772cfd0
Mitch V <=mvandy33@gmail.com>
2024-10-15 10:39:33 -0600

    Completed base verify account spec

--- tests/verify-account.spec.ts
+++ tests/verify-account.spec.ts
@@ -1,29 +1,33 @@
-import test, { expect } from "@playwright/test";
+import test, { expect, Page } from "@playwright/test";
 
 const path = 'https://qaplayground.dev/apps/verify-account/';
 const code = '999999';
 
 test.describe('Account verification demo', () => {
+    
+    let page: Page;
+
     test.beforeAll(async ({ browser }) => {
-        await (await browser.newPage()).goto(path);
+        page = await browser.newPage()
+        await page.goto(path);
     });
 
-    test('should display the correct title', async ({ page }) => {
-        await page.goto(path);
+    test('should display the correct title', async () => {
         expect(await page.locator('#title').innerText())
             .toBe('Verify Your Account');
     });
 
-    test('should display the valid code', async ({page}) => {
+    test('should display the valid code', async () => {
         let labelText = await page.locator('[class*="info"]').innerText();
         let displayedCode = labelText.match(/\d/g)?.join('');
         expect(displayedCode).toBe(code);
     });
 
-    test('should submit the valid code successfully', async ({page}) => {
+    test('should submit the valid code successfully', async () => {
         let inputs = await page.locator('input[class*="code"]').all();
         for(let i = 0; i < inputs.length; i++) {
-            await inputs[i].fill(code[i]);
+            // Must use type() instead of fill() here to properly submit the code
+            await inputs[i].type(code[i]);
         }
         expect(await page.locator('[class*="info"]').innerText())
             .toBe('Success');
